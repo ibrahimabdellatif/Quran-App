14f6a733b1b8cd141d5655797d0a60d9250d10d9
ibrahim <ibrahimabdellatif186@gmail.com>
2021-09-09 02:23:51 +0200

    add fragments and connect it with main Activity

--- app/src/main/java/com/ibrahim/quranapp/MainActivity.kt
+++ app/src/main/java/com/ibrahim/quranapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ibrahim.quranapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
 }
