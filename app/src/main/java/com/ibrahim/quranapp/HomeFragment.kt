a1900022317e14b0299b3c23253b8d0f99c44d09
ibrahim <ibrahimabdellatif186@gmail.com>
2021-09-10 19:22:41 +0200

    test api to get and show size of total list item of readers

--- app/src/main/java/com/ibrahim/quranapp/HomeFragment.kt
+++ app/src/main/java/com/ibrahim/quranapp/HomeFragment.kt
@@ -4,15 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ibrahim.quranapp.Adapters.HomeAdapter
+import com.ibrahim.quranapp.network.QuranApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeFragment : Fragment() {
 
     var recyclerView: RecyclerView? = null
     var layoutManager: RecyclerView.LayoutManager? = null
+    var testTextView:TextView?=null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +30,8 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView(view)
+        testTextView = view.findViewById(R.id.tv_test)
+        initRetrofit()
     }
 
     private fun recyclerView(view: View) {
@@ -52,4 +60,21 @@ class HomeFragment : Fragment() {
         QuranData(0, "مشاري بن راشد", "", "حفص عن عاصم"),
 
         )
+
+     fun initRetrofit(){
+        QuranApi.retrofitService.getData().enqueue(object :Callback<List<QuranData>>{
+            override fun onResponse(
+                call: Call<List<QuranData>>,
+                response: Response<List<QuranData>>
+            ) {
+                testTextView?.text = response.body()?.size.toString()
+            }
+
+            override fun onFailure(call: Call<List<QuranData>>, t: Throwable) {
+                testTextView?.text = "this is failure state " + t.message
+            }
+
+
+        })
+    }
 }
---
+++ app/src/main/java/com/ibrahim/quranapp/network/QuranApiService.kt
@@ -0,0 +1,36 @@
+package com.ibrahim.quranapp.network
+
+import com.ibrahim.quranapp.QuranData
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+//   api/reciters/  >> this for get method
+private const val BASE_URL = "https://qurani-api.herokuapp.com/"
+
+//init moshi for retrofit
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+//init retrofit
+private val retrofit = Retrofit
+    .Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * create public interface to call [getData]
+ */
+interface QuranApiService {
+    @GET("api/reciters/")
+     fun getData(): Call<List<QuranData>>
+}
+
+object QuranApi {
+    val retrofitService: QuranApiService by lazy { retrofit.create(QuranApiService::class.java) }
+}
